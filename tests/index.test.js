9a108cc1c517e02e1654ac969ceb2a9dfb228227
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-04-12 22:35:35 +0530

    User signUp Endpoint

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,26 @@
-function sum (a, b) {
-    return a + b
-}
+const  axios  = require("axios");
 
 
-test('adds 1 +2 to equal 3',() => {
-    let ans = sum(1, 2)
-    expect(ans).toBe(3)
+
+
+const BACKEND_URL = "http://localhost:3000"
+
+describe("Authentication", () => {
+    test('User is able to sign up only once', async() => {
+        const username = "Shiruvati" + Math.random();
+        const password = "123445"
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username,
+            password,
+            type: "admin"
+        })
+        expect(response.status).toBe(200)
+        const updatedResponse = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username,
+            password,
+            type: "admin"
+        })
+        expect(updatedResponse.status).toBe(400)
+    })
 })
+
